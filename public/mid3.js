d6bce60b6bb2d5fcf45728d899f67b2dd45b753b
namjin <domino8788@naver.com>
2020-05-04 11:00:30 +0900

    201432003-6

--- public/mid3.js
+++ public/mid3.js
@@ -3,7 +3,7 @@ let persons1 = []
 for(let i=0;i<3;i++)
     persons1[i] = {name:"홍길동", age:16+i}
 
-let persons2 = persons1
+let persons2 = persons1.slice(0)
 
 persons1[0].age = 20
 
--- public/mid5.js
+++ public/mid5.js
@@ -3,5 +3,5 @@ let arr = []
 for(let i=0;i<5;i++){
     arr.push(Math.floor(Math.random()*100+1))
 }
-console.log(arr)
-console.log(arr.map(value=>!(value%2)).reduce((a,b)=> a+b))
+
+console.log(arr.reduce((a,b)=> (a%2==0?a:0)+(b%2==0?b:0)))
