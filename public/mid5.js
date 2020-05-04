7c265cc1ea186552f8b726d47c44891ff1bbf660
namjin <domino8788@naver.com>
2020-05-04 10:40:20 +0900

    201432003-5

--- public/mid5.js
+++ public/mid5.js
@@ -1,6 +1,7 @@
 let arr = []
 
-for(let i=0;i<100;i++){
+for(let i=0;i<5;i++){
     arr.push(Math.floor(Math.random()*100+1))
 }
 console.log(arr)
+console.log(arr.map(value=>!(value%2)).reduce((a,b)=> a+b))
